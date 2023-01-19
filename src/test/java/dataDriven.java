0cbaaccb23bdc35b8f33301a85fc98fa26cb971c
hunadim <hmapulane@gmail.com>
2023-01-19 20:38:51 +0200

    implemented while loop to rows and cells and checking cell string text

--- src/test/java/dataDriven.java
+++ src/test/java/dataDriven.java
@@ -1,13 +1,16 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class dataDriven {
     public static void main(String[] args) throws IOException {
 
-        FileInputStream fis = new FileInputStream("/Users/hunadimapulane/DemoDocs/ddtDemo.xlxs");
+        FileInputStream fis = new FileInputStream("/Users/hunadimapulane/DemoDocs/ddtDemo.xlsx");
         XSSFWorkbook workbook = new XSSFWorkbook(fis); //accepts file input stream argument
 
         int sheets = workbook.getNumberOfSheets(); //to get count of sheets in excel doc
@@ -16,11 +19,21 @@ public class dataDriven {
         {
             if(workbook.getSheetName(i).equalsIgnoreCase("testcases")) {
                 XSSFSheet sheet = workbook.getSheetAt(i);
+                //get specific row
+                Iterator<Row> rows = sheet.iterator();
+                Row firstRow = rows.next();
+                //get specific cell
+                Iterator<Cell> getCell = firstRow.cellIterator();
+                while(getCell.hasNext()) //checks if cell has next but won't move
+                {
+                    Cell value = getCell.next(); //use .next() to move to cell
+                    if(value.getStringCellValue().equalsIgnoreCase("testcases"));
+                    {
+                        //action retrieve data
+                    }
+
+                }
             }
-
-
         }
-
-
     }
 }
